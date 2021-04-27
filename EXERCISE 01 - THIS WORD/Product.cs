dd03607355fb3bd333f027524d56d037564adc91
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-27 14:17:11 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 01 - THIS WORD/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EXERCISE_01___THIS_WORD{
+    //----------------------START | OBJ RETANGULO------------------------//
+    class Product {
+        public string Name;
+        public double Price;
+        public int Amount;
+        public Product(string name, double price)  {
+            Name = name;
+            Price = price;            
+        }
+        public Product(string name, double price, int amount) : this(name, price) {
+            Amount = amount;
+        }
+        //------------------------- MET. CLASS ----------------------------//
+        public double TotalValueStock(){
+            return Price * Amount;
+        }
+        public void AddProduct(int amount) {
+            Amount += amount;
+        }
+        public void RemoveProduct(int amount) {
+            Amount -= amount;
+        }
+        public override string ToString() {
+            return Name
+                + "| $ " + Price.ToString("F2") + "| " + Amount + "  Unidades | Total: $ " + TotalValueStock().ToString("F2");
+        }     
+    }
+    //--------------------------- END --------------------------------//
+}
